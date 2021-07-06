fe1e5770d5973959d89cd891aee63278a1967b87
Rlyown <czy426@icloud.com>
2021-07-07 01:58:39 +0800

    enhance the function of run_kernel.sh to support the creation of initramfs

--- run_kernel.sh
+++ run_kernel.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
-# run kernel in qemu for debug
-qemu-system-x86_64 -s -kernel ./vmlinux -initrd ./initramfs.cpio.gz -nographic -append "console=ttyS0 $*"
+
+
+print_help() {
+    echo "Usage: run_kernel.sh [-a <append>] -m <method>"
+    echo "    -h: Show this message."
+    echo "    -a: append cmdline argument to the kernel running in the qemu. This argument should set before -m."
+    echo "    -m: Choose method."
+    echo "         'debug': Start a qemu virtual machine."
+    echo "         'init': Package the initramfs directory to the initramfs.cpio.gz. It used in qemu."
+}
+
+if [ $# -eq 0 ]
+then
+    print_help
+fi
+
+while getopts "hm:a:" optname
+do
+    case "$optname" in
+        "h")
+            print_help
+            ;;
+        "a")
+            CMDLINE="$OPTARG"
+            ;;
+        "m")
+            if [ "$OPTARG"x = "debug"x ]
+            then
+                echo run kernel in qemu for debug
+                qemu-system-x86_64 --enable-kvm -s -kernel ./vmlinux -initrd ./initramfs.cpio.gz -nographic -append "console=ttyS0 $CMDLINE"
+            elif [ "$OPTARG"x = "init"x ]
+            then
+                echo package the initramfs
+                cd initramfs
+                find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
+                cd -
+            fi
+            ;;
+        "?")
+            print_help
+            ;;
+    esac
+done
+
